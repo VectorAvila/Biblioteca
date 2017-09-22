428a9cc6781515b8694d8acae9dcf8f92405c2c2
jcamacho <jcamacho@L0002338.vectoritcgroup.com>
2017-09-22 13:59:41 +0200

    fix bug loan

--- src/main/java/com/example/prueba/loan/service/LoanServiceImpl.java
+++ src/main/java/com/example/prueba/loan/service/LoanServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.prueba.loan.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
@@ -7,13 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.prueba.book.dao.repository.IBookRepository;
+import com.example.prueba.book.service.BookServiceImpl;
 import com.example.prueba.loan.dao.entity.LoanEntity;
 import com.example.prueba.loan.dao.repository.ILoanRepository;
 import com.example.prueba.loan.exception.LoanException;
 import com.example.prueba.loan.model.LoanDTO;
 import com.example.prueba.loan.model.LoanMapper;
-import com.example.prueba.person.dao.repository.IPersonRepository;
+import com.example.prueba.person.service.PersonServiceImpl;
 
 @Service
 public class LoanServiceImpl {
@@ -25,44 +26,44 @@ public class LoanServiceImpl {
 	LoanMapper loanMapper;
 	
 	@Autowired
-	IBookRepository bookRepository;
+	BookServiceImpl bookServiceImpl;
 	
 	@Autowired
-	IPersonRepository personRepository; 
+	PersonServiceImpl personServiceImpl;
 
 	@Transactional
 	public List<LoanDTO> getLoans() {
 		List<LoanEntity> listLoanEntities = (List<LoanEntity>) loanRepository.findAll();
-		return loanMapper.toObject(listLoanEntities);
+		return fillDatas(listLoanEntities);
 	}
 
 	@Transactional
 	public List<LoanDTO> getLoanByDniAndIsbn(String dni, Integer isbn) {
-		List<LoanEntity> loanEntity = loanRepository.findByDniAndIsbn(dni, isbn);
-		return loanMapper.toObject(loanEntity);
+		List<LoanEntity> listLoanEntities = loanRepository.findByDniAndIsbn(dni, isbn);
+		return fillDatas(listLoanEntities);
 	}
 
 	@Transactional
 	public List<LoanDTO> getLoanByDni(String dni) {
-		List<LoanEntity> loanEntity = loanRepository.findByDni(dni);
-		return loanMapper.toObject(loanEntity);
+		List<LoanEntity> listLoanEntities = loanRepository.findByDni(dni);
+		return fillDatas(listLoanEntities);
 	}
 
 	@Transactional
 	public List<LoanDTO> getLoanByIsbn(Integer isbn) {
-		List<LoanEntity> loanEntity = loanRepository.findByIsbn(isbn);
-		return loanMapper.toObject(loanEntity);
+		List<LoanEntity> listLoanEntities = loanRepository.findByIsbn(isbn);
+		return fillDatas(listLoanEntities);
 	}
 
 	@Transactional
 	public LoanDTO addLoan(LoanDTO loan) throws LoanException {
-		if (bookRepository.findByIsbn(loan.getBook().getIsbn()) != null
-			&& personRepository.findByDni(loan.getPerson().getDni()) != null) {
+		if (bookServiceImpl.getBook(loan.getBook().getIsbn()) != null
+			&& personServiceImpl.getPerson(loan.getPerson().getDni()) != null) {
 			List<LoanEntity> loansEntity = loanRepository.findByDniAndIsbn(loan.getPerson().getDni(), loan.getBook().getIsbn());
 			if (CollectionUtils.isEmpty(loansEntity)) {
 				LoanEntity loanEntity = loanMapper.toEntity(loan);
 				loanEntity = loanRepository.save(loanEntity);
-				return loanMapper.toObject(loanEntity);
+				return fillData(loanEntity);
 			} else {
 				throw new LoanException();
 			}
@@ -80,4 +81,22 @@ public class LoanServiceImpl {
 			throw new LoanException();
 		}
 	}
+	
+	private List<LoanDTO> fillDatas(List<LoanEntity> entities) {
+		List<LoanDTO> retorno = new ArrayList<>();
+		if (CollectionUtils.isNotEmpty(entities)) {
+			for (LoanEntity entity : entities) {
+				retorno.add(fillData(entity));
+			}
+		}
+		return retorno;
+	}
+	
+	private LoanDTO fillData(LoanEntity entity) {
+		LoanDTO loanDTO = loanMapper.toObject(entity);
+		loanDTO.setBook(bookServiceImpl.getBook(entity.getIsbn()));
+		loanDTO.setPerson(personServiceImpl.getPerson(entity.getDni()));
+		return loanDTO;
+	}
+	
 }
